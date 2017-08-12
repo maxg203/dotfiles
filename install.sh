d02682c0f0d9c7eb811e6f5dc8e79be2a7be0831
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-12 22:00:45 +0100

    Fix git clone command

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ cd
 config config --local status.showUntrackedFiles no
 echo ".cfg" >> .gitignore
 
-config clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo -ne '\n' | vim -c 'PluginInstall' -c 'qa!'
