af3964a415aca770339a864f670ab8f674e73720
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-12 21:13:22 +0100

    Do not use Vundle as a submodule for now

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ echo ".cfg" >> .gitignore
 
 config config --local status.showUntrackedFiles no
 config checkout --force
-config submodule init
-config submodule update
 
+config clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c 'PluginInstall' -c 'qa!'
