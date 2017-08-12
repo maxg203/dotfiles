6b9227822af8885a905ed7360c5c0f0c2002821c
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-12 21:29:46 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 cd
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-echo ".cfg" >> .gitignore
 
 config config --local status.showUntrackedFiles no
 config checkout --force
+echo ".cfg" >> .gitignore
 
 config clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -c 'PluginInstall' -c 'qa!'
+echo -ne '\n' | vim -c 'PluginInstall' -c 'qa!'
