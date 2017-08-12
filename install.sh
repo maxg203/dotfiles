785fedbd6a2cb59929f73de229cec698d12b659c
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-12 22:34:23 +0100

    Added nicer install script output

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@ cd
 config config --local status.showUntrackedFiles no
 echo ".cfg" >> .gitignore
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null
+echo
+echo "Don't worry about vim complaining... we're just installing vim plugins!"
 echo -ne '\n' | vim -c 'PluginInstall' -c 'qa!'
+echo
+echo "Thank you for installing Max's dotfiles! Enjoy."
