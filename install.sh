892aed2e765b9fd7bdcc3d5227bdfddd4183367c
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-12 20:46:48 +0100

    Modified dotfile install script

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 cd
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 echo ".cfg" >> .gitignore
-git clone --bare https://github.com/maxg203/dotfiles-2.git $HOME/.cfg
 
 config config --local status.showUntrackedFiles no
 config checkout --force
