6a11534c6b0db007b5938bb80049b493f76244e2
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-12 21:55:12 +0100

    Reduced install script, still better than nothing

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 cd
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 config config --local status.showUntrackedFiles no
-config checkout --force
 echo ".cfg" >> .gitignore
 
 config clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
