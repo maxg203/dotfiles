42795e9b9f04f7449d29b3317b5286297a2bafa0
Max Goodridge <max.goodridge@maplecroft.com>
2017-09-01 15:14:18 +0100

    Remove old deprecated setup script

--- install.sh
+++ install.sh
@@ -7,5 +7,9 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim >
 echo
 echo "Don't worry about vim complaining... we're just installing vim plugins!"
 echo -ne '\n' | vim -c 'PluginInstall' -c 'qa!'
+
+# Consider switching to zsh, will need to install oh-my-zsh if that happens
+# sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'
+
 echo
 echo "Thank you for installing Max's dotfiles! Enjoy."
