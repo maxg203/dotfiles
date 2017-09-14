b516b51afbcc1206b386b29af32bb9d65bdf1a44
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-09-14 03:15:10 +0100

    Test oh my zsh installation process with interactive shell highlighting

--- .zshrc
+++ .zshrc
@@ -10,7 +10,7 @@ export ZSH=/Users/Max/.oh-my-zsh
 ZSH_THEME=""
 export DISABLE_AUTO_UPDATE="true"
 
-plugins=(git)
+plugins=(git, zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 autoload -U promptinit; promptinit
--- install.sh
+++ install.sh
@@ -6,13 +6,12 @@ if ![ -f ~/.gitignore ]; then
 fi
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'
+
 echo
 echo "Don't worry about vim complaining... we're just installing vim plugins!"
 echo -ne '\n' | vim -c 'PluginInstall' -c 'qa!'
-
-# Consider switching to zsh, will need to install oh-my-zsh if that happens
-# sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'
 
 echo
 echo "Thank you for installing Max's dotfiles! Enjoy."
