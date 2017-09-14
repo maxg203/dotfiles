414d849777b6e9bfb3ee80ffeeced0021633516b
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-09-14 03:00:40 +0100

    Add interactive shell command highlighting

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ config config --local status.showUntrackedFiles no
 echo ".cfg" >> .gitignore
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo
 echo "Don't worry about vim complaining... we're just installing vim plugins!"
 echo -ne '\n' | vim -c 'PluginInstall' -c 'qa!'
