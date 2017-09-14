0f1df098870764f59ef6b204857ef07dbb9f77f1
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-09-14 22:53:53 +0100

    Fix antigen installation

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ fi
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null
 
 sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'
-curl -L git.io/antigen > antigen.zsh
+curl -L git.io/antigen > ~/.antigen.zsh
 
 
 echo
