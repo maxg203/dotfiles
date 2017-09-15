89b6db99fdd3a5b3db19aeae968f8bb9b1298c8b
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-09-16 00:43:19 +0100

    Tidy up the install script
    Tidy up the install script after having that epiphany to curl the install script off GitHub and pipe it in to shell

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
+#!/usr/bin/env sh
 cd
 source ~/.aliases
-git clone --bare https://github.com/maxg203/dotfiles.git $HOME/.cfg
+
+# Git
+config clone --bare https://github.com/maxg203/dotfiles.git $HOME/.cfg
 config checkout --force
-
-source ~/.provision/install_dependencies.sh
-
 config config --local status.showUntrackedFiles no
+config config --local help.autocorrect 1
 if [ -f ~/.gitignore ]; then
     echo "Existing gitignore file was not modified."
 else
     echo ".cfg" >> .gitignore
 fi
 
+# Install dependencies
+source ~/.provision/install_dependencies.sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim >/dev/null
-
 sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
-curl -L git.io/antigen > ~/.antigen.zsh
+curl -L git.io/antigen > ~/.antigen.zsh && echo
 
-
-echo
+# Vim
 echo "Don't worry about vim complaining... we're just installing vim plugins!"
-echo -ne '\n' | vim -c 'PluginInstall' -c 'qa!'
+echo -ne '\n' | vim -c 'PluginInstall' -c 'qa!' && echo
 
-echo
 echo "Thank you for installing Max's dotfiles! Enjoy."
