7762d56c0f6aba76adb9b5585a8f456ce11723ec
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-09-16 01:06:10 +0100

    Add a more user friendly output (also helps with install script debugging)

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 cd
+echo "CONFIGURING ALIASES..."
 source ~/.aliases
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-# Git
+echo "CLONING DOTFILES FROM GITHUB..."
 config clone --bare https://github.com/maxg203/dotfiles.git $HOME/.cfg
+echo "DO GIT THINGS..."
 config checkout --force
 config config --local status.showUntrackedFiles no
 config config --local help.autocorrect 1
@@ -14,14 +16,14 @@ else
     echo ".cfg" >> .gitignore
 fi
 
-# Install dependencies
+echo "INSTALL DEPENDENCIES..."
 source ~/.provision/install_dependencies.sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim >/dev/null
 sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
 curl -L git.io/antigen > ~/.antigen.zsh && echo
 
-# Vim
+echo "INSTALL VIM PLUGINS..."
 echo "Don't worry about vim complaining... we're just installing vim plugins!"
 echo -ne '\n' | vim -c 'PluginInstall' -c 'qa!'
 echo
