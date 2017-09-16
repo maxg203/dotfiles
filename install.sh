dcbe415cbc8ef8885cc4051b2274ecbe68ccae96
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-09-16 02:49:22 +0100

    'source' command is a bash thing!

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 cd ~/
 
 echo && echo "CLONING DOTFILES FROM GITHUB..."
@@ -16,7 +16,6 @@ else
 fi
 
 echo && echo "CONFIGURING ALIASES..."
-# XXX: Can't source scripts until after installation has finished for some reason
 source ~/.aliases
 
 echo && echo "INSTALLING DEPENDENCIES..."
