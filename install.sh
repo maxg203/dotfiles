8086a49f52828419ec9a47a30a69f66a79bfa31e
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-09-16 01:11:43 +0100

    Add new lines to install script output

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 cd
-echo "CONFIGURING ALIASES..."
+echo && echo "CONFIGURING ALIASES..."
 source ~/.aliases
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-echo "CLONING DOTFILES FROM GITHUB..."
+echo && echo "CLONING DOTFILES FROM GITHUB..."
 config clone --bare https://github.com/maxg203/dotfiles.git $HOME/.cfg
-echo "DO GIT THINGS..."
+echo && echo "DO GIT THINGS..."
 config checkout --force
 config config --local status.showUntrackedFiles no
 config config --local help.autocorrect 1
@@ -16,16 +16,15 @@ else
     echo ".cfg" >> .gitignore
 fi
 
-echo "INSTALL DEPENDENCIES..."
+echo && echo "INSTALL DEPENDENCIES..."
 source ~/.provision/install_dependencies.sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim >/dev/null
 sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
 curl -L git.io/antigen > ~/.antigen.zsh && echo
 
-echo "INSTALL VIM PLUGINS..."
+echo && echo "INSTALL VIM PLUGINS..."
 echo "Don't worry about vim complaining... we're just installing vim plugins!"
 echo -ne '\n' | vim -c 'PluginInstall' -c 'qa!'
-echo
 
-echo "Thank you for installing Max's dotfiles! Enjoy."
+echo && echo "Thank you for installing Max's dotfiles! Enjoy."
